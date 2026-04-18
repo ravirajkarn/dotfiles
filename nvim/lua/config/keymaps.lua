40ee297ff2c4e87cf73a99b2bfa8500c8c3f17d8
Sumit Kumar <ravirajkarn@outlook.com>
2026-04-18 11:45:27 +0530

    custom nvim keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,13 +2,28 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-local M = {}
+function Map(mode, lhs, rhs, opts)
+  local options = { noremap = true, silent = true }
+  if opts then
+    options = vim.tbl_extend("force", options, opts)
+  end
+  vim.keymap.set(mode, lhs, rhs, options)
+end
 
-M.general = {
-  n = {
-    ["<C-h"] = { "<cmd> TmuxNavigateLeft<CR>", "window left" },
-    ["<C-l"] = { "<cmd> TmuxNavigateRight<CR>", "window right" },
-    ["<C-j"] = { "<cmd> TmuxNavigateDown<CR>", "window down" },
-    ["<C-k"] = { "<cmd> TmuxNavigateUp<CR>", "window up" },
-  },
-}
+vim.g.mapleader = " "
+
+Map("n", "<C-h>", "<C-w>h")
+Map("n", "<C-j>", "<C-w>j")
+Map("n", "<C-k>", "<C-w>k")
+Map("n", "<C-l>", "<C-w>l")
+
+--terminal
+Map("t", "<C-h>", "<cmd>wincmd h<CR>")
+Map("t", "<C-j>", "<cmd>wincmd j<CR>")
+Map("t", "<C-k>", "<cmd>wincmd k<CR>")
+Map("t", "<C-l>", "<cmd>wincmd l<CR>")
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
+Map("v", "<", "<gv")
+Map("v", ">", ">gv")
